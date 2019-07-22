21a50c98818021f2df3a650d83d6ab0e71bc9e96
Nzubechukwu Nnamani <nzubechukwunnamani@Nzubechukwus-MacBook-Pro.local>
2019-07-22 17:37:34 +0100

    ft(homepage) add the homepage [Finishes #166466671]

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Index from "./pages/index";
+import AdminLogin from "./pages/adminLogin";
+
+import { Route, Switch } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Index} />
+        <Route exact path="/adminLogin" component={AdminLogin} />
+      </Switch>
+    </>
+  );
+};
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules:  [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
@@ -22,6 +22,16 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    } 
+                }]
             }
         ]
     },
